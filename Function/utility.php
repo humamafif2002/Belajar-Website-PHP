1e6c8a9336d8cbcc57cd8ba5639d2cce787acb99
Humam Afif <Humamafif2002@gmail.com>
2024-02-24 16:59:44 +0700

    Menambahkan Function

---
+++ Function/utility.php
@@ -0,0 +1,21 @@
+<?php
+// debugging:
+// var_dump():
+// Apakah ada :
+// isset();
+// Apakah kosong:
+// empty()
+// Menghentikan proses:
+// die()
+// Menjeda:
+// sleep
+
+
+// USER DEFINED FUNCTION
+
+function salam($nama)
+{
+  return "Selamat pagi $nama";
+}
+
+echo salam('Nero');
