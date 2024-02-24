9ff6f2dfe51fe6b77e6a5f186e7825863b131983
Humam Afif <Humamafif2002@gmail.com>
2024-02-24 17:11:52 +0700

    Menambahkan Array

---
+++ Array/Array.php
@@ -0,0 +1,13 @@
+<?php
+
+// Array 1 dimensi 
+$hari = ['Senin', 'Selasa', 'Rabu'];
+var_dump($hari);
+
+
+// Array 2 dimensi /multidimensi
+$Mahasiswa = [
+  ['Humam afif', '15210107', 'Teknik Informatika'],
+  ['Mutia Amanda', '15210108', 'Kedokteran']
+];
+var_dump($Mahasiswa);
