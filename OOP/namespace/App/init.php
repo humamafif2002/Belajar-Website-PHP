c4fc96c787375724fbceae6c92fdafeced69e2b9
Humam Afif <Humamafif2002@gmail.com>
2024-03-05 22:02:34 +0700

    Menambahkan beberapa catatan

--- OOP/namespace/App/init.php
+++ OOP/namespace/App/init.php
@@ -1,4 +1,8 @@
 <?php
+// JIKA 1 FILE TELAH DIJADIKAN NAMESPACE,MAKA $CLASS AKAN MENGISI NAMESPACE PADA LINK ALIAS SEPERTI INI :
+// xampp/htdocs/Website PHP/Belajar-Website-PHP/OOP/namespace/App/Produk/App\Produk\User.php
+// karna sudah diberi namespace maka alamat file berubah
+
 
 // untuk menggunakan namespace gunakan ini
 
@@ -11,7 +15,7 @@ spl_autoload_register(function ($class) {
   // hasilnya adalah 
   // $class = ["App","Produk","User"] maka akan kita ambil yang paling terakhir,yang mana pasti class
   $class = end($class);
-//  maka require di bawah adalah class yang terakhir yaitu "User";
+  //  maka require di bawah adalah class yang terakhir yaitu "User";
   require_once __DIR__ . '/Produk/' . $class . ".php";
 });
 
@@ -23,6 +27,6 @@ spl_autoload_register(function ($class) {
   // hasilnya adalah 
   // $class = ["App","Produk","User"] maka akan kita ambil yang paling terakhir,yang mana pasti class
   $class = end($class);
-//  maka require di bawah adalah class yang terakhir yaitu "User";
+  //  maka require di bawah adalah class yang terakhir yaitu "User";
   require_once __DIR__ . '/Service/' . $class . ".php";
 });
