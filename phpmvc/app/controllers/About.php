6c6f63f53dfa5534bff9cc9e717f69abb7c4ee25
Humam Afif <Humamafif2002@gmail.com>
2024-03-08 07:35:52 +0700

    Menambahkan materi Controller

---
+++ phpmvc/app/controllers/About.php
@@ -0,0 +1,15 @@
+<?php
+
+class About
+{
+
+  public function index()
+  {
+    echo 'About/index';
+  }
+
+  public function makan()
+  {
+    echo 'About/makan';
+  }
+}
--- phpmvc/app/core/App.php
+++ phpmvc/app/core/App.php
@@ -3,10 +3,50 @@
 class App
 {
 
+  protected $controller = 'Home',
+    $method = 'index',
+    $params = [];
+
+
   public function __construct()
   {
     $url = $this->parseURL();
-    var_dump($url);
+    // var_dump($url);
+    // controller
+    if (isset($url)) {
+      if (file_exists('../app/controllers/' . $url[0] . '.php')) {
+        $this->controller = $url[0];
+        unset($url[0]);
+      }
+    }
+
+
+
+    require_once "../app/controllers/{$this->controller}.php";
+    $this->controller = new $this->controller;
+    // alamat tujuan dimulai dari file index,karna yang dijalankan adalah index.php,jadi kita harus keluar
+
+
+
+    // Method
+    if (isset($url[1])) {
+      if (method_exists($this->controller, $url[1])) {
+        $this->method = $url[1];
+        unset($url[1]);
+      } else {
+        unset($url[1]);
+      }
+    }
+
+
+    // Parameter
+    if (!empty($url)) {
+      $this->params = array_values($url);
+    }
+
+    var_dump($this->controller, $this->method, $this->params);
+    // jalankan controller & method ,serta kirimkan params jika ada
+    call_user_func_array([$this->controller, $this->method], $this->params);
   }
 
   public function parseURL()
