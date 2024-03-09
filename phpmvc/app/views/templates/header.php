0b8d748d109f2ffcba5043919dffad39f1e23082
Humam Afif <Humamafif2002@gmail.com>
2024-03-10 03:48:49 +0700

    Memberikan style pada mvc

--- phpmvc/app/views/templates/header.php
+++ phpmvc/app/views/templates/header.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $data['title']; ?></title>
+  <link href="http://localhost/Website%20PHP/Belajar-Website-PHP/phpmvc/public/css/bootstrap.css" rel="stylesheet">
 </head>
 
 <body>
